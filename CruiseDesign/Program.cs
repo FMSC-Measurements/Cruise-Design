9e4305424cb2e39d77434919dd3d5dc2bb4ece51
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-10-16 16:38:17 -0500

    Add service locator and add LoggingService and DialogService

--- CruiseDesign/Program.cs
+++ CruiseDesign/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿using CruiseDAL.V3.Sync.Syncers;
+using CruiseDesign.Design_Pages;
+using CruiseDesign.Historical_setup;
+using CruiseDesign.ProductionDesign;
+using CruiseDesign.Reports;
+using CruiseDesign.Services;
+using CruiseDesign.Services.Logging;
+using CruiseDesign.Stats;
+using CruiseDesign.Strata_setup;
+using FMSC.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -7,6 +20,8 @@ namespace CruiseDesign
 {
     static class Program
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -21,7 +36,58 @@ namespace CruiseDesign
 #endif
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(ConfigureServices)
+                .ConfigureLogging(ConfigureLogging).Build();
+            ServiceProvider = host.Services;
+
             Application.Run(new CruiseDesignMain(args));
         }
+
+        private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+        {
+            builder.AddAppCenterLogger();
+        }
+
+        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            //example
+            //services.AddTransient<IMyService, MyService>();
+            //services.AddTransient<MyForm>();
+
+            // register all forms
+            services.AddTransient<CreateProduction>();
+            services.AddTransient<DesignMain>();
+            services.AddTransient<DesignSaveForm>();
+            services.AddTransient<MethodSelect>();
+            services.AddTransient<Processing>();
+
+            services.AddTransient<SaleSetupPage>();
+
+            services.AddTransient<ProductionDesignMain>();
+            services.AddTransient<ReportAdditional>();
+            services.AddTransient<ReportForm>();
+            services.AddTransient<ReportViewer>();
+
+            services.AddTransient<userSelectStratum>();
+
+            services.AddTransient<TDV_Select>();
+            services.AddTransient<Working>();
+
+            services.AddTransient<TestBox1>();
+            services.AddTransient<CostSetupForm>();
+            services.AddTransient<CruiseDesignMain>();
+            services.AddTransient<HistoricalSetupWizard>();
+
+            services.AddTransient<StrataSetupWizard>();
+            services.AddTransient<WaitForm>();
+
+            // register other services
+            services.AddSingleton<IDialogService, DialogService>();
+
+        }
+
     }
 }
---
+++ CruiseDesign/Services/DialogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace CruiseDesign.Services
+{
+    public class DialogService : IDialogService
+    {
+        public void ShowMessage(string message, string caption = "")
+        {
+            MessageBox.Show(message, caption);
+        }
+    }
+
+    public interface IDialogService
+    {
+        void ShowMessage(string message, string caption = "");
+    }
+}
