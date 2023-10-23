e57283f746a9c98ffded59d28497ba3f09fa3941
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-10-23 17:24:45 -0500

    add SelectCreateProductionV3TemplateDialog

---
+++ CruiseDesign/Dialogs/SelectCreateProductionV3TemplateDialog.cs
@@ -0,0 +1,113 @@
+﻿using CruiseDesign.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CruiseDesign.Dialogs
+{
+    public partial class SelectCreateProductionV3TemplateDialog : Form
+    {
+        private string _templatePath;
+        private string _reconFilePath;
+
+        public SelectCreateProductionV3TemplateDialog()
+        {
+            InitializeComponent();
+
+            TemplatePath = null;
+            DialogService = Program.ServiceProvider.GetRequiredService<IDialogService>();
+        }
+
+        public SelectCreateProductionV3TemplateDialog(string reconFilePath)
+            : this()
+        {
+            ReconFilePath = reconFilePath;
+        }
+
+        public IDialogService DialogService { get; }
+
+        public string TemplatePath
+        {
+            get => _templatePath;
+            set
+            {
+                _templatePath = value;
+                _templatePathTextBox.Text = value?.ToString();
+            }
+        }
+
+        public string ReconFilePath
+        {
+            get => _reconFilePath;
+            set
+            {
+                _reconFilePath = value;
+                _reconFilePathTextBox.Text = value?.ToString();
+            }
+        }
+
+        public string SelectedSourcePath
+        {
+            get
+            {
+                if (_useTemplateFileRadioButton.Checked)
+                {
+                    return TemplatePath;
+                }
+                else
+                {
+                    return ReconFilePath;
+                }
+            }
+        }
+
+        private void _browseTemplateButton_Click(object sender, EventArgs e)
+        {
+            var result = DialogService.AskOpenV3TemplateFile();
+            if (result != null)
+            {
+                if (!File.Exists(result))
+                {
+                    DialogService.ShowMessage("File Not Found");
+                    return;
+                }
+
+                TemplatePath = result;
+            }
+        }
+
+        private void _okButton_Click(object sender, EventArgs e)
+        {
+            var selectedSource = SelectedSourcePath;
+            if (selectedSource == null)
+            {
+                DialogService.ShowMessage("No File Selected");
+                return;
+            }
+
+            if (!File.Exists(selectedSource))
+            {
+                DialogService.ShowMessage("Selected File Not Found");
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void _cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void _useReconFileRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void _useTemplateFileRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
--- CruiseDesign/Services/DialogService.cs
+++ CruiseDesign/Services/DialogService.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using CruiseDesign.Dialogs;
+using System;
+using System.Windows.Forms;
 
 namespace CruiseDesign.Services
 {
@@ -8,10 +10,61 @@ namespace CruiseDesign.Services
         {
             MessageBox.Show(message, caption);
         }
+
+        public string AskOpenV3TemplateFile(string initialDir = null)
+        {
+            initialDir ??= Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\CruiseFiles\\Templates";
+
+            using var fileDialog = new OpenFileDialog()
+            {
+                Filter = "V3 Template File (*.crz3t)|*.crz3t",
+                InitialDirectory = initialDir,
+            };
+
+            if (fileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return null;
+            }
+
+            return fileDialog.FileName;
+        }
+
+        public string AskSaveProductionFilePath(bool defaultV3, string defaultFileName)
+        {
+            var fileDialog = new SaveFileDialog()
+            {
+                DefaultExt = (defaultV3) ? ".crz3" : ".cruise",
+                Filter = "V2 Cruise File|*.cruise|V3 Cruise File|*.crz3",
+                FileName = defaultFileName,
+            };
+
+            if(fileDialog.ShowDialog() != DialogResult.OK) { return null; }
+
+            return fileDialog.FileName;
+        }
+
+        public string AskSelectCreateProductionV3Template(string reconFilePath)
+        {
+            using var dialog = new SelectCreateProductionV3TemplateDialog(reconFilePath);
+
+            var result = dialog.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                return dialog.SelectedSourcePath;
+            }
+            return null;
+        }
     }
 
     public interface IDialogService
     {
         void ShowMessage(string message, string caption = "");
+
+        string AskOpenV3TemplateFile(string initialDir = null);
+
+        string AskSaveProductionFilePath(bool defaultV3, string defaultFileName);
+
+        string AskSelectCreateProductionV3Template(string reconFilePath);
     }
 }
