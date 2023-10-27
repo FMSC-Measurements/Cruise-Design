331d4b4fdd7821b1ce8226ba3b6639ebecdd57b0
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-10-26 20:38:19 -0500

    remove background worker form Processing

--- CruiseDesign/Design_Pages/Processing.Designer.cs
+++ CruiseDesign/Design_Pages/Processing.Designer.cs
@@ -28,42 +28,40 @@
       /// </summary>
       private void InitializeComponent()
       {
-         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Processing));
-         this.pictureBox1 = new System.Windows.Forms.PictureBox();
-         this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-         this.SuspendLayout();
-         // 
-         // pictureBox1
-         // 
-         this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-         this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-         this.pictureBox1.Name = "pictureBox1";
-         this.pictureBox1.Size = new System.Drawing.Size(285, 103);
-         this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-         this.pictureBox1.TabIndex = 0;
-         this.pictureBox1.TabStop = false;
-         // 
-         // Processing
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(285, 103);
-         this.Controls.Add(this.pictureBox1);
-         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-         this.Name = "Processing";
-         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-         this.Text = "Processing";
-         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-         this.ResumeLayout(false);
-         this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Processing));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Processing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 103);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Processing";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
       }
 
       #endregion
 
       private System.Windows.Forms.PictureBox pictureBox1;
-      private System.ComponentModel.BackgroundWorker backgroundWorker1;
    }
 }
