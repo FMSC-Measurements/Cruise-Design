25f7b25140a5ea0d86bdd3af080831d28fc6f272
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2024-01-16 17:54:18 -0600

    WIP fixing issue with Sqlite Library Loading

--- CruiseDesign/Program.cs
+++ CruiseDesign/Program.cs
@@ -25,6 +25,8 @@ namespace CruiseDesign
         [STAThread]
         private static void Main(string[] args)
         {
+            SQLitePCL.Batteries_V2.Init();
+
 #if !DEBUG
             Microsoft.AppCenter.AppCenter.Start(Secrets.CRUISEDESIGN_APPCENTER_KEY_WINDOWS,
                                typeof(Microsoft.AppCenter.Analytics.Analytics), typeof(Microsoft.AppCenter.Crashes.Crashes));
